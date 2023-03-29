5e3a27725bbf0682d7b1613893e8af2758785ead
rokHladin <rh16274@student.uni-lj.si>
2023-03-29 20:13:16 +0200

    Project with multiple files

---
+++ multiple_files_1/main.c
@@ -0,0 +1,6 @@
+#include "parameter.c"
+
+int main() {
+	printParameter(7);
+	return 0;
+}
---
+++ multiple_files_1/parameter.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void printParameter(int param) {
+	printf("I printParameter, was just called with parameter %d", param);
+}
